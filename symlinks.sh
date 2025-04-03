fb1c888361df558a7e89fbd6c01eef358ed66dc2
Sokmeta-Reth <metasokreth@gmail.com>
2025-04-03 19:29:06 +0900

    feat: create symlink function
    - create bootstrap script
    - create script create symlink

---
+++ symlinks.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+# This file stores the symlinks to be created.
+
+DOTFILES_ROOT=$(pwd -P)
+
+# The format is:
+# <source_path>:<destination_path>
+# The source path is relative to the DOTFILES_ROOT directory.
+# The destination path is the absolute path where the symlink will be created.
+declare -A symlinks=(
+  ["$DOTFILES_ROOT/zsh/zshrc"]="$HOME/.zshrc"
+)
